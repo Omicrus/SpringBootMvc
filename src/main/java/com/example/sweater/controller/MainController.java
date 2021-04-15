abbc4d118b32f21fb0c9c746d2b547d35a54fb48
Boris Slabari <omicrus@gmail.com>
2021-04-15 14:14:02 +0200

    Initial commit

---
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -0,0 +1,104 @@
+package com.example.sweater.controller;
+
+import com.example.sweater.domain.Message;
+import com.example.sweater.domain.Role;
+import com.example.sweater.domain.User;
+import com.example.sweater.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+
+@Controller
+public class MainController {
+    @Autowired
+    private MessageRepo messageRepo;
+
+    //Spring put in the string variable uploadPath - the path saved in the properties file
+    @Value("${upload.path}")
+    private String uploadPath;
+
+
+    @GetMapping("/")
+    public String greeting(@AuthenticationPrincipal User user, Model model) {
+
+        model.addAttribute("user", user);
+        model.addAttribute("isLogged", user != null);
+        return "greeting";
+    }
+
+    @GetMapping("/main")
+    public String main(@AuthenticationPrincipal User user,
+                       @RequestParam(required = false, defaultValue = "") String filter,
+                       Model model) {
+        Iterable<Message> messages = messageRepo.findAll();
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+        model.addAttribute("isAdmin", user.getRoles().contains(Role.ADMIN));
+        model.addAttribute("user", user);
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
+        return "main";
+    }
+
+    @PostMapping("/main")
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag,
+            @RequestParam("file") MultipartFile file,
+            Model model) throws IOException {
+
+
+        Message message = new Message(text, tag, user);
+
+        if (file != null & !file.isEmpty()) {
+            File uploadDir = new File(uploadPath);
+
+            if (!uploadDir.exists()) {
+                uploadDir.mkdir();
+            }
+
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFileName = uuidFile + "." + file.getOriginalFilename();
+
+            file.transferTo(new File(uploadPath + "/" + resultFileName));
+
+            message.setFilename(resultFileName);
+        }
+
+        messageRepo.save(message);
+        Iterable<Message> messages = messageRepo.findAll();
+        model.addAttribute("isAdmin", user.getRoles().contains(Role.ADMIN));
+        model.addAttribute("user", user);
+        model.addAttribute("messages", messages);
+        return "main";
+    }
+
+    //удаляем 1 сообшение из БД и возвращяем обновлёный список сообшений
+    @GetMapping(value = "/main", params = "messageId")
+    public String delete(@RequestParam(value = "messageId", required = false) long id,
+                         @AuthenticationPrincipal User user,
+                         Model model) {
+
+        messageRepo.deleteById(id);
+        Iterable<Message> messages = messageRepo.findAll();
+        model.addAttribute("isAdmin", user.getRoles().contains(Role.ADMIN));
+        model.addAttribute("user", user);
+        model.addAttribute("message", messages);
+        return "redirect:/main";
+    }
+
+
+}
